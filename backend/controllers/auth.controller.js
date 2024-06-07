48aa511232e5d04316c33c7e21fa4c91b7530e06
saied83 <saiedtechit@gmail.com>
2024-06-08 00:14:15 +0600

    create signIn api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -44,8 +44,28 @@ const signUp = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-const signIn = (req, res) => {
-  res.send("this is signIn Page");
+const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPassCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPassCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signin controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 const signOut = (req, res) => {
   res.send("this is signOut Page");
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (UserId, res) => {
     maxAge: 10 * 24 * 60 * 60 * 1000, // ms
     httpOnly: true, //Prevent XSS attacks cross-site scripting attacks
     sameSites: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: true,
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
