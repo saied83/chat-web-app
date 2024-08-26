2ec73dc9fe925477246958e692859ac24a872a68
saied83 <saiedtechit@gmail.com>
2024-08-26 15:03:53 +0600

    change socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chat-web-app-guai.onrender.com/", {
         query: {
           userId: authUser._id,
         },
