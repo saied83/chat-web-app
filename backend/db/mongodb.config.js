cf12be48d1077c5e1958820c525853e5efd6e5b9
saied83 <saiedtechit@gmail.com>
2024-08-26 14:48:14 +0600

    build app for deployment

--- backend/db/mongodb.config.js
+++ backend/db/mongodb.config.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const mongodbConfig = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URI);
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,14 @@ import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+import dotenv from "dotenv";
 
+dotenv.config();
 // rest object
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
@@ -20,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", index.html));
+});
 
 // listen
 server.listen(PORT, () => {
