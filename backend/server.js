f4e0b26e101c8078501d6911be60ed20e6a8c5d3
saied83 <saiedtechit@gmail.com>
2024-08-26 10:45:15 +0600

    add socket into backend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ import mongodbConfig from "./db/mongodb.config.js";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
+import { app, server } from "./socket/socket.js";
 
 // rest object
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
@@ -22,7 +22,7 @@ app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
 
 // listen
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   mongodbConfig();
   console.log(`Server Running on port ${PORT}`.bgMagenta.white);
 });
